3281cce688bcea290b8009541f1fdadde57efd84
Luiz Fernando <medeirosluizf@gmail.com>
2016-05-04 13:34:31 -0300

    Building initial project.

---
+++ SistInt_HW1/NeuralNet.py
@@ -0,0 +1,61 @@
+'''
+This is the NN project applied to MNIST. The initial goal is to practice NN concepts
+with the MNIST lib of images. First test will be composed of simple backpropagation 
+machines.
+
+The algorithm for this simple, backpropagation, perceptron based NN is as follows:
+     
+     * Retrieve input files (MNIST in this case);
+     * process the images into an input array;
+     * Add a cell to the input array, totaling 1, in order to represent the bias;
+     * Establish the number of initial hidden layers;
+          // In this case we are starting with 1;
+     * Based on the number of Hidden layers, set up the weight array;
+          * Weight array should be something of the input, and the respective number of hidden layers;
+               // This is for iteration through the array. 
+          * Randomize all Ws. 
+     // Now establishing an algo for 1 Hidden Layer specific case:
+     
+     * Set up 10 neurons; // each representing a number;
+     * set up a W(sizeOfInputIMGs+1,1); // the +1 is for the bias;
+     * Input Transposed times W + Bias;
+     * Whichever Neuron has the highest signal wins;
+     
+     // Training the network;
+     // Produce the back propagation;
+     
+     * Start running trainng Data samples;
+     * Compare the result of the winning neuron with the actual result;
+     * Calculate error;
+     * Propagate the error back through the network in order to adjust; // Check respective error methods
+     * Adjust Weights;
+     * Run next sample;
+     * Do this until overall testing error reaches a very low number (acceptable error);
+     
+     
+
+'''
+
+
+class NeuralNetwork(object):
+     # This is the constructor;
+     
+     def __init__(self,X=0):
+          print("Constructor called;");
+          
+          
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SistInt_HW1/test0.py
@@ -0,0 +1,12 @@
+import numpy as np;
+import matplotlib.pyplot as plt;
+
+
+a=np.arange(0,100);
+#a.shape;
+
+print ("working");
+print (a);
+plt.plot(a,a);
+
+plt.show();
